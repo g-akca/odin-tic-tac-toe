4d67775ef9c70326d37112c753e84a6c23444947
g-akca <gok.akca66@gmail.com>
2026-03-07 03:19:29 +0300

    Add methods for playing a round and checking win or tie

--- script.js
+++ script.js
@@ -16,13 +16,15 @@ const Gameboard = (() => {
     return { getBoard, resetBoard, fillBoard };
 })();
 
-const Player = (name, marker) => {
-    return { name, marker };
+const Player = (name, mark) => {
+    return { name, mark };
 }
 
 const GameController = (() => {
     let player1, player2, currentPlayer;
     let gameOver = false;
+
+    const isGameOver = () => gameOver;
 
     const startGame = (player1Name, player1Mark, player2Name, player2Mark, starter) => {
         player1 = Player(player1Name, player1Mark);
@@ -34,7 +36,37 @@ const GameController = (() => {
         Gameboard.resetBoard();
     }
 
-    return { startGame };
+    const playRound = (index) => {
+        if (gameOver) return;
+
+        const filled = Gameboard.fillBoard(index, currentPlayer.mark);
+        if (!filled) return;
+
+        if (checkWin()) {
+            console.log(`${currentPlayer.name} won!`);
+            gameOver = true;
+            return;
+        }
+
+        if (checkTie()) {
+            console.log("Tie!");
+            gameOver = true;
+            return;
+        }
+
+        currentPlayer = currentPlayer == player1 ? player2 : player1;
+    }
+
+    const checkWin = () => {
+        const board = Gameboard.getBoard();
+        const winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+        return winningCombos.some(combo => combo.every(i => board[i] == currentPlayer.mark));
+    };
+
+    const checkTie = () => Gameboard.getBoard().every(cell => cell != "");
+
+    return { startGame, playRound, isGameOver };
 })();
 
 const DisplayController = (() => {
